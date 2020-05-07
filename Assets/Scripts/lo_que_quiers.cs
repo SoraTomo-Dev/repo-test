f1ecccd003afcede2f37badb4b0c1bac68185a02
Raúl Rivadeneyra <yujiko@soratomo.team>
2020-05-07 16:28:19 -0500

    Balba

--- Assets/Scripts/lo_que_quiers.cs
+++ Assets/Scripts/lo_que_quiers.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class lo_que_quiers : MonoBehaviour
 {
     public float hola_mundo = 4f;
-    public float hola_patata = 4f;
     // Start is called before the first frame update
     void Start()
     {
